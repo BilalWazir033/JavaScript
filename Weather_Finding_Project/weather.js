cca7a978490613524c51fe3d36544469c29fdbb9
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-09-19 18:14:52 +0500

    Added weather.js file for fetching and displaying weather data based on user input.

--- Weather_Finding_Project/weather.js
+++ Weather_Finding_Project/weather.js
@@ -1,11 +1,43 @@
 const apiKey="665b5a5e8fb0a17487e82d95c70e271e";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=peshawar";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox=document.querySelector(".search input");
+const searchBtn=document.querySelector(".search button");
+const weatherIcon=document.querySelector(".weather-icon");
 
-async function checkWeather(){
-    const response=await fetch(apiUrl+`&appid=${apiKey}`);
+async function checkWeather(city){
+    const response=await fetch(apiUrl+ city + `&appid=${apiKey}`);
     var data=await response.json();
-    console.log(data);
+
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp)+"°C";
+
+
+    if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="images/clouds.png";
+    }
+    else if(data.weather[0].main=="Clear"){
+        weatherIcon.src="images/clear.png";
+    }
+    else if(data.weather[0].main=="Rain"){
+        weatherIcon.src="images/rain.png";
+    }
+    else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src="images/drizzle.png";
+    }
+    else if(data.weather[0].main=="Mist"){
+        weatherIcon.src="images/mist.png";
+    }
+    else if(data.weather[0].main=="Snow"){
+        weatherIcon.src="images/snow.png";
+    }
+
+
+    document.querySelector(".weather").style.display="block";
+    
 }
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+})  
 
 
 
@@ -15,5 +47,5 @@ async function checkWeather(){
 
 
 git add .
-git commit -m "creating a weather finding project using openweathermap api and html,css and javascript. App work by using city name and it will show the weather of that city."
+git commit -m "Added weather.js file for fetching and displaying weather data based on user input."
 git push -u origin main
